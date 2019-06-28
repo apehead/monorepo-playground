2f6c88cc480c328bb4db27fceaad09d7923647d1
Alexander Curiel <alexander@spotahome.com>
2019-06-28 18:30:19 +0200

    fix(git): fix ignored files by Git
    affects: @apehead/awesome-tool, @apehead/print-time

--- packages/awesome-tool/dist/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.run=void 0;const run=()=>"Running";exports.run=run;
-//# sourceMappingURL=index.js.map
--- packages/print-time/dist/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.printTime=void 0;const printTime=()=>{const dateTime=new Date;return dateTime.toISOString()};exports.printTime=printTime;
-//# sourceMappingURL=index.js.map
